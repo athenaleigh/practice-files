a6a33b59805a3ba29f94a8377ef468a1a8ef44c2
athenaleigh <66501771+athenaleigh@users.noreply.github.com>
2020-07-04 15:38:12 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -3,7 +3,7 @@ function displayDate(){
   }
 
   // Set the date we're counting down to
-var countDownDate = new Date("Jul 17, 2020 09:00:00 UTC-0400").getTime();
+var countDownDate = new Date("Jul 30, 2020 UTC-0400").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
