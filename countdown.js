48aed31f0e3af41b10954d97fcb812d4d2868053
athenaleigh <66501771+athenaleigh@users.noreply.github.com>
2020-07-04 15:48:28 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -2,7 +2,7 @@ function displayDate(){
     document.getElementById("current").innerHTML=Date();
   }
 
-  // Set the date we're counting down to
+  /* Set the date we're counting down to
 var countDownDate = new Date("Jul 30, 2020 UTC-0400").getTime();
 
 // Update the count down every 1 second
@@ -30,123 +30,53 @@ var x = setInterval(function() {
     document.getElementById("persLaunch").innerHTML = "LAUNCH!";
   }
 }, 1000);
+*/
 
 
 
+// Perseverance launch
+var countDownDate = new Date("Jul 30, 2020 UTC-0400").getTime();
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jul 17, 2020 09:00:00 UTC-0400").getTime();
-
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("persLaunch").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("persLaunch").innerHTML = "LAUNCH!";
   }
 }, 1000);
 
-// Perseverence touchdown
+// Perseverance touchdown
 var landingDate = new Date("Feb 18, 2021 UTC-0400").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   var distance = landingDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
   var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("persLand").innerHTML = weeks + "w " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("persLand").innerHTML = "TOUCHDOWN!";
-  }
-}, 1000);
-
-// SpaceX launch
-var spaceXLaunch = new Date("May 30, 2020 15:22:00 UTC-0400").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = spaceXLaunch - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("spacex").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("spacex").innerHTML = "We have Liftoff!";
-  }
-}, 1000);
-
-// ISS docking
-var ISSdock = new Date("May 31, 2020 10:27:00 UTC-0400").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = ISSdock - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("docking").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("docking").innerHTML = "Docked!";
   }
 }, 1000);
